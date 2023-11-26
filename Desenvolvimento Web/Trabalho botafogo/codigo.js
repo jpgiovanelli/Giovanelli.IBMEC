6c3c0f75625dc9a2faef026da785adb049751c7d
Giovanelli <joaopedrogiovanelli@gmail.com>
2023-11-26 15:12:17 -0300

    trab botafogo

--- Desenvolvimento Web/Trabalho botafogo/codigo.js
+++ Desenvolvimento Web/Trabalho botafogo/codigo.js
@@ -1,167 +1,175 @@
 
-const body = document.body
-body.style.display = 'flex'
-body.style.flexDirection = 'column'
-body.style.alignItems = 'center'
+const seguir = localStorage.getItem('senha')
+const div_primaria = document.getElementById('options')
 
-const handle = (e) => {
-    let text_value = String(e.target.value) 
-    if (text_value == 'Todos') {
-        text_value = 'all'
-    }
-    else {
-        text_value = text_value.toLowerCase()
-    }
-    console.log(text_value)
-    fetch_api(text_value)
+if (seguir == '81dc9bdb52d04dc20036dbd8313ed055'){
 
-}
+    const select_ = document.createElement('select') 
 
-const select = document.getElementById('menuopt')
-select.addEventListener('change', handle)
+    select_.id = 'menuopt'
+    select_.className = 'menu'
 
-const main = (atletas) => {
+    var option_ = document.createElement('option')
+    option_.text = 'Todos'
+    select_.appendChild(option_)
 
-    const tamanho = atletas.length
+    var option_ = document.createElement('option')
+    option_.text = 'Masculino'
+    select_.appendChild(option_)
 
-    var element = document.getElementById('info')
-    element.style.display = 'flex'
-    element.style.flexWrap = 'wrap'
-    element.style.justifyContent = 'space-between'
+    var option_ = document.createElement('option')
+    option_.text = 'Feminino'
+    select_.appendChild(option_)
 
-    for (var i = 0; i < tamanho; i++) {
-        var div = document.createElement('article')
-        div.id = `atleta ${i}` 
-        div.style.background = "gray"
-        div.style.color = "white"
-        div.style.width = "20rem"
-        div.style.display = 'flex'
-        div.style.flexDirection = 'column'
-        div.style.margin = '10px'
+    div_primaria.appendChild(select_)
 
-        div.dataset.id = atletas[i].id
-        div.dataset.altura = atletas[i].altura
-        div.dataset.nome_completo = atletas[i].nome_completo
-        div.dataset.nascimento = atletas[i].nascimento
+    const body = document.body
+    body.style.display = 'flex'
+    body.style.flexDirection = 'column'
+    body.style.alignItems = 'center'
 
-        div.onclick = click
+    const handle = (e) => {
+        let text_value = String(e.target.value) 
+        if (text_value == 'Todos') {
+            text_value = 'all'
+        }
+        else {
+            text_value = text_value.toLowerCase()
+        }
+        console.log(text_value)
+        fetch_api(text_value)
 
-        var tag = document.createElement('p')
-        var text = document.createTextNode(atletas[i].nome)
-        tag.style.textAlign = 'center'
-        tag.appendChild(text)
-        div.appendChild(tag)
-
-        var tag2 = document.createElement('img')
-        tag2.src = atletas[i].imagem
-        tag2.alt = atletas[i].nome
-        div.appendChild(tag2)
-
-        var tag3 = document.createElement('p')
-        var text = document.createTextNode(atletas[i].descricao)
-        tag3.style.textAlign = 'center'
-        tag3.appendChild(text)
-        div.appendChild(tag3)
-
-        element.appendChild(div)
     }
 
-}
+    const select = document.getElementById('menuopt')
+    select.addEventListener('change', handle)
 
-const click = (e) => {
-    const artigo = e.target.closest('article')
+    const main = (atletas) => {
 
-    document.cookie = `id=${artigo.dataset.id}`
-    document.cookie = `altura=${artigo.dataset.altura}`
-    document.cookie = `nome_completo=${artigo.dataset.nome_completo}`
+        const tamanho = atletas.length
 
-    localStorage.setItem('id', artigo.dataset.id)
-    localStorage.setItem('altura', artigo.dataset.altura)
-    localStorage.setItem('nome_completo', artigo.dataset.nome_completo)
-    localStorage.setItem('nascimento', artigo.dataset.nascimento)
+        var element = document.getElementById('info')
+        element.style.display = 'flex'
+        element.style.flexWrap = 'wrap'
+        element.style.justifyContent = 'space-between'
 
-    sessionStorage.setItem('id', artigo.dataset.id)
-    sessionStorage.setItem('altura', artigo.dataset.altura)
-    sessionStorage.setItem('nome_completo', artigo.dataset.nome_completo)
-    sessionStorage.setItem('nascimento', artigo.dataset.nascimento)
+        for (var i = 0; i < tamanho; i++) {
+            var div = document.createElement('article')
+            div.id = `atleta ${i}` 
+            div.style.background = "gray"
+            div.style.color = "white"
+            div.style.width = "20rem"
+            div.style.display = 'flex'
+            div.style.flexDirection = 'column'
+            div.style.margin = '10px'
 
-    console.log(localStorage.getItem('nascimento'))
-    console.log(sessionStorage.getItem('nome_completo'))
-}
+            div.dataset.id = atletas[i].id
+            div.dataset.altura = atletas[i].altura
+            div.dataset.nome_completo = atletas[i].nome_completo
+            div.dataset.nascimento = atletas[i].nascimento
 
-const acha_cookie = (chave) => {
-    const lista = document.cookie.split('; ')
-    const procurado = lista.find( (e) => e.startsWith(chave))
+            div.onclick = click
 
-    return procurado.split('=')[1]
-}
+            var tag = document.createElement('p')
+            var text = document.createTextNode(atletas[i].nome)
+            tag.style.textAlign = 'center'
+            tag.appendChild(text)
+            div.appendChild(tag)
 
-const variar_texto = async () => {
-    try {
-        const texto_carregado = document.getElementById('texto carregado');
-        if (!texto_carregado) {
-            console.error("Element with ID 'texto carregado' not found");
-            return;
+            var tag2 = document.createElement('img')
+            tag2.src = atletas[i].imagem
+            tag2.alt = atletas[i].nome
+            div.appendChild(tag2)
+
+            var tag3 = document.createElement('p')
+            var text = document.createTextNode(atletas[i].descricao)
+            tag3.style.textAlign = 'center'
+            tag3.appendChild(text)
+            div.appendChild(tag3)
+
+            element.appendChild(div)
         }
 
-        let contador = 1;
+    }
 
-        setInterval(() => {
-            let texto = 'Carregando';
+    const click = (e) => {
+        const artigo = e.target.closest('article')
+        const id = artigo.dataset.id
+    
+        window.location.href = `/detalhes.html?id=${id}`
+    }
 
-            if (contador === 1) {
-                texto_carregado.innerHTML = texto;
-            } else if (contador === 2) {
-                texto_carregado.innerHTML = texto + '.';
-            } else if (contador === 3) {
-                texto_carregado.innerHTML = texto + '..';
-            } else {
-                texto_carregado.innerHTML = texto + '...';
+    const variar_texto = async () => {
+        try {
+            const texto_carregado = document.getElementById('texto carregado');
+            if (!texto_carregado) {
+                console.error("Element with ID 'texto carregado' not found");
+                return;
             }
 
+            let contador = 1;
 
-            contador = contador % 4 + 1; // Volta para 1 quando chega a 3
-        }, 60); // Atualiza a cada 500 milissegundos (meio segundo)
-    } catch (error) {
-        console.error("An error occurred:", error);
-    }
-};
+            setInterval(() => {
+                let texto = 'Carregando';
 
-const fetch_api = (page) => {
+                if (contador === 1) {
+                    texto_carregado.innerHTML = texto;
+                } else if (contador === 2) {
+                    texto_carregado.innerHTML = texto + '.';
+                } else if (contador === 3) {
+                    texto_carregado.innerHTML = texto + '..';
+                } else {
+                    texto_carregado.innerHTML = texto + '...';
+                }
 
-    var element = document.getElementById('info')
 
-    try {
-        while (element.firstChild) {
-            element.removeChild(element.firstChild);
+                contador = contador % 4 + 1; // Volta para 1 quando chega a 3
+            }, 60); // Atualiza a cada 500 milissegundos (meio segundo)
+        } catch (error) {
+            console.error("An error occurred:", error);
         }
-    } catch (error) {
-        
+    };
+
+    const fetch_api = (page) => {
+
+        var element = document.getElementById('info')
+
+        try {
+            while (element.firstChild) {
+                element.removeChild(element.firstChild);
+            }
+        } catch (error) {
+            
+        }
+
+        var carregando = document.createElement('div')
+        carregando.id = 'Carregando'
+        var h1_carregando = document.createElement('h1')
+        h1_carregando.id = 'texto carregado'
+
+        carregando.appendChild(h1_carregando)
+        select.parentElement.appendChild(carregando)
+
+        variar_texto()
+
+        const promise = fetch(`https://botafogo-atletas.mange.li/${page}`)
+
+        promise.then( async (response) => {
+                const atletas =  await response.json()
+                main(atletas)
+                const wait = document.getElementById('Carregando')
+                wait.parentNode.removeChild(wait)
+            }
+        )
     }
 
-    var carregando = document.createElement('div')
-    carregando.id = 'Carregando'
-    var h1_carregando = document.createElement('h1')
-    h1_carregando.id = 'texto carregado'
 
-    carregando.appendChild(h1_carregando)
-    select.parentElement.appendChild(carregando)
 
-    variar_texto()
-
-    const promise = fetch(`https://botafogo-atletas.mange.li/${page}`)
-
-    promise.then( async (response) => {
-            const atletas =  await response.json()
-            main(atletas)
-            const wait = document.getElementById('Carregando')
-            wait.parentNode.removeChild(wait)
-        }
-    )
+    fetch_api('all')
 }
 
-
-
-fetch_api('all')
+else {
+    window.location.href = '/'
+}
 
 
--- Desenvolvimento Web/Trabalho botafogo/response.js
+++ Desenvolvimento Web/Trabalho botafogo/response.js
@@ -1,11 +1,27 @@
-const key = 'AIzaSyCCMOIQ531QdncjyHtAgNecv7zhF6ARKoA'
-const cep = '22790-704'
 
-const url = `https://maps.googleapis.com/maps/api/geocode/json?key=${key}&address=${cep}`
+localStorage.setItem('senha', '')
 
-const promise = fetch(url)
+const form = document.getElementById('form')
+const button = document.createElement('button')
+button.innerText = 'Ir'
 
-promise.then(response => response.json()).then(location => {
-    console.log(location)
+button.addEventListener('click', () => {
+
+    const password_text = document.getElementById('password').value
+
+    const md5_password = md5(password_text)
+    
+    if (md5_password == '81dc9bdb52d04dc20036dbd8313ed055') {
+        localStorage.setItem('senha', '81dc9bdb52d04dc20036dbd8313ed055')
+        window.location.href = 'logado.html'
+    }
+    else {
+        alert('Senha errada')
+    }
+    
+
 })
+
+form.appendChild(button)
+
 
